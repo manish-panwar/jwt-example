02c166037cb7abbe3be26f650243f0fced28a598
manishk <Water@1234>
2016-04-06 17:59:12 -0400

    Add example unit test with RsaKeySpec and public key modulus.

--- src/test/java/jwt/JwtExample.java
+++ src/test/java/jwt/JwtExample.java
@@ -4,9 +4,17 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.impl.crypto.RsaProvider;
 import org.junit.Test;
 
+import java.math.BigInteger;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Date;
 
 import static java.time.Instant.now;
@@ -17,8 +25,12 @@ import static org.junit.Assert.assertEquals;
  */
 public class JwtExample {
 
+    public static final String AUDIENCE = "SEG:some_fake_location_group_id";
+    public static final String ISSUER = "Console";
+
     @Test
     public void testSymmetric() throws Exception {
+
 
         // Generate JWT Auth header using symmetric key(shared secret key between 2 systems). This is simulating a client.
         String symmetricKey = "shHAiduj848^(#$)";
@@ -29,8 +41,8 @@ public class JwtExample {
         // Validate JWT auth header. This is simulating server side component.
         Jws<Claims> jws = Jwts.parser().setSigningKey(symmetricKey.getBytes()).parseClaimsJws(authHeader);
         assertEquals(SignatureAlgorithm.HS256.getValue(), jws.getHeader().getAlgorithm());
-        assertEquals("Java_SEG", jws.getBody().getId());
-        assertEquals("manish", jws.getBody().getIssuer());
+        assertEquals(AUDIENCE, jws.getBody().getAudience());
+        assertEquals(ISSUER, jws.getBody().getIssuer());
     }
 
     @Test
@@ -46,18 +58,46 @@ public class JwtExample {
         // Validate JWT auth header using public key. This is simulating server side component.
         Jwts.parser()
                 .setSigningKey(keyPair.getPublic())
-                .requireAudience("some_audience")
-                .requireIssuer("manish")
+                .requireAudience(AUDIENCE)
+                .requireIssuer(ISSUER)
                 .parseClaimsJws(authHeader);
     }
 
+    @Test
+    public void testWithRsaPublicKeySpec() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        // Create RSA Key pair.
+        KeyPair keyPair = RsaProvider.generateKeyPair(2048);
+
+        // This is how Console will generate the JWT token. Same token will be passed as "Authorization" header by Console.
+        String jwtToken = getJwtBuilder()
+                .signWith(SignatureAlgorithm.RS256, keyPair.getPrivate())
+                .compact();
+
+        // Get the modulus and exponent from RSA public key. This is what will be given to SEG via REST API or during the installation.
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+        String modulus = Base64.getEncoder().encodeToString(rsaPublicKey.getModulus().toByteArray());
+        String exponent = Base64.getEncoder().encodeToString(rsaPublicKey.getPublicExponent().toByteArray());
+
+        // Below code shows how SEG will use the Modulus and exponent that are returned by REST API/Console
+        RSAPublicKeySpec rsaPubKey = new RSAPublicKeySpec(new BigInteger(1, Base64.getDecoder().decode(modulus)),
+                new BigInteger(1, Base64.getDecoder().decode(exponent)));
+        PublicKey pubKey = KeyFactory.getInstance("RSA").generatePublic(rsaPubKey);
+
+        Jws claims = Jwts.parser()
+                .setSigningKey(pubKey)
+                .requireAudience(AUDIENCE)
+                .requireIssuer(ISSUER)
+                .parseClaimsJws(jwtToken);
+        System.out.println(claims);
+
+    }
+
     private JwtBuilder getJwtBuilder() {
-        Instant expiry = now().plus(10, ChronoUnit.SECONDS);
+        Instant expiry = now().plus(3650, ChronoUnit.DAYS);
         return Jwts.builder()
                 .setExpiration(Date.from(expiry))
-                .setSubject("Some subject")
-                .setAudience("some_audience")
-                .setId("Java_SEG")
-                .setIssuer("manish");
+                .setAudience(AUDIENCE)
+                .setIssuer(ISSUER);
     }
 }
